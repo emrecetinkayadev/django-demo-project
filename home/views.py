94bb25c4bbd483593d2f8fda60f013a8c073a896
emrecetinkayadev <emrecetinkayadev@gmail.com>
2023-04-04 14:35:25 +0300

    Notes and Home added to our project.

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
+ 
