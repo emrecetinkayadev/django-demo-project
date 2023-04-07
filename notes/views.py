74fc0babaec905beef4cfd1a8fa1daee168314c6
emrecetinkayadev <emrecetinkayadev@gmail.com>
2023-04-07 15:31:18 +0300

    Note Forms are added.

--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView
 
+from .forms import NotesForm
 from .models import Notes
 
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NoteCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        return render(request, 'notes/notes_404.html', {})
-    return render(request, 'notes/notes_detail.html', {'note': note})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = 'notes/notes_list.html'
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+
+
+# def list_view(response):
+#     notes = Notes.objects.all()
+#     return render(response, 'notes/notes_list.html', {'notes': notes})
