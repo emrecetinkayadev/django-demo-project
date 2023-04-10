5a7a30be9198a578866aade3dda22ebe49835325
emrecetinkayadev <emrecetinkayadev@gmail.com>
2023-04-10 11:38:30 +0300

    final version.

--- home/views.py
+++ home/views.py
@@ -5,6 +5,30 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwards):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwards)
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
 
 
 class HomeView(TemplateView):
